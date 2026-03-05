12e5407d4f15c7fec2cc47317e7b839ef53c039d
LucasBorenstein <borensteinlucas@gmail.com>
2026-03-05 19:10:01 -0300

    sigo

--- main.js
+++ main.js
@@ -15,3 +15,30 @@ function conectar(){
 document.getElementById("panelLuces").style.display="flex"
 
 }
+//interruptor
+const toggle = document.getElementById("modoOscuro")
+
+// cargar preferencia guardada
+
+if(localStorage.getItem("modo") === "oscuro"){
+
+document.body.classList.add("dark-mode")
+toggle.checked = true
+
+}
+
+toggle.addEventListener("change",()=>{
+
+if(toggle.checked){
+
+document.body.classList.add("dark-mode")
+localStorage.setItem("modo","oscuro")
+
+}else{
+
+document.body.classList.remove("dark-mode")
+localStorage.setItem("modo","claro")
+
+}
+
+})
